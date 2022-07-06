24301f827212a2e9b8b22537dadffaa10e9258e0
HotPotatoC <juandotulung@gmail.com>
2022-07-06 21:41:03 +0700

    refactor: improved runge kutta implementation

--- differential_equations.py
+++ differential_equations.py
@@ -2,34 +2,30 @@ from numbers import Number
 from typing import Callable
 
 
-def runge_kutta_2nd_order(
-    x: Number, initial_x: Number, y: Number, dy: Callable, h: Number
-):
-    while initial_x != x:
-        k1 = dy(x, y)
-        k2 = dy(x + h, y + h * k1)
+def runge_kutta_2nd_order(x0: Number, y0: Number, x: Number, dy: Callable, n: Number):
+    h = (x - x0) / n
+    for _ in range(n):
+        k1 = dy(x0, y0)
+        k2 = dy(x0 + h, y0 + h * k1)
 
-        solution = y + h / 2 * (k1 + k2)
-        initial_x += h
-        y = solution
+        y0 = y0 + h / 2 * (k1 + k2)
+        x0 += h
 
-    return solution
+    return y0
 
 
-def runge_kutta_4th_order(
-    x: Number, initial_x: Number, y: Number, dy: Callable, h: Number
-):
-    while initial_x != x:
-        k1 = dy(x, y)
-        k2 = dy(x + h / 2, y + h / 2 * k1)
-        k3 = dy(x + h / 2, y + h / 2 * k2)
-        k4 = dy(x + h, y + h * k3)
+def runge_kutta_4th_order(x0: Number, y0: Number, x: Number, dy: Callable, n: Number):
+    h = (x - x0) / n
+    for _ in range(n):
+        k1 = dy(x0, y0)
+        k2 = dy(x0 + h / 2, y0 + h / 2 * k1)
+        k3 = dy(x0 + h / 2, y0 + h / 2 * k2)
+        k4 = dy(x0 + h, y0 + h * k3)
 
-        solution = y + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
-        initial_x += h
-        y = solution
+        y0 = y0 + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+        x0 += h
 
-    return solution
+    return y0
 
 
 if __name__ == "__main__":
@@ -45,12 +41,12 @@ if __name__ == "__main__":
 
     dθ = lambda t, θ: -2.2067 * 10 ** (-12) * (θ**4 - 81 * 10**8)
 
-    h = 48
+    n = 10
     initial_t = 0
     initial_θ = 1200
 
-    R2 = lambda x: runge_kutta_2nd_order(x, initial_t, initial_θ, dθ, h)
-    R4 = lambda x: runge_kutta_4th_order(x, initial_t, initial_θ, dθ, h)
+    R2 = lambda x: runge_kutta_2nd_order(initial_t, initial_θ, x, dθ, n)
+    R4 = lambda x: runge_kutta_4th_order(initial_t, initial_θ, x, dθ, n)
 
     print(f"θ(480) 2nd Order: {R2(480)} K")
     print(f"θ(480) 4th Order: {R4(480)} K")
